57c5051fc29c063e625a319db18960c69e2eb637
Saurabh Kumar <saurav123sau123@gmail.com>
2022-05-17 07:12:23 +0530

    Error Middleware

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/db.js'
 import colors from 'colors'
 
 import productRoutes from './routes/productRoutes.js'
+import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 
 dotenv.config();
 connectDB();
@@ -12,11 +13,8 @@ connectDB();
 const app = express();
 
 app.use('/api/products',productRoutes)
-
-app.get('/',(req,res)=>{
-    res.send("API is running...")
-})
-
+app.use(notFound)
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 5000
