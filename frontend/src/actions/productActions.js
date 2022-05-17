a965af08d3e5352fe0564f68df73830b060b1f09
Saurabh Kumar <saurav123sau123@gmail.com>
2022-05-17 12:52:39 +0530

    Prduct Details reducer & actions added

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -17,3 +17,21 @@ export const listProducts = () => async (dispatch) => {
         })
     }
 }
+
+export const listProductDetails = (id) => async (dispatch) => {
+    try {
+        dispatch({type: 'PRODUCT_DETAILS_REQUEST'})
+
+        const { data } = await axios.get(`/api/products/${id}`)
+
+        dispatch({
+            type: 'PRODUCT_DETAILS_SUCCESS',
+            payLoad: data
+        })
+    } catch (error) {
+        dispatch({
+            type: 'PRODUCT_DETAILS_FAIL',
+            payLoad: error.response && error.response.data.message ? error.response.data.message : error.message
+        })
+    }
+}
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,10 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
-import { productListReducer } from './reducers/productReducers.js'
+import { productListReducer, productDetailsReducer } from './reducers/productReducers.js'
 
 const reducer = combineReducers({
-    productList: productListReducer
+    productList: productListReducer,
+    productDetails: productDetailsReducer
 })
 
 const initialState = {}
