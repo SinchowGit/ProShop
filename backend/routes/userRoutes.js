4762c866444fb65fd81e5c7868ac25c24e3a06eb
Saurabh Kumar <saurav123sau123@gmail.com>
2022-05-18 16:41:30 +0530

    Tokens added , ProtectMiddleware added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express'
 const router = express.Router()
 
-import { authUser } from '../controllers/userControllers.js'
+import { protect } from '../middleware/authMiddleware.js'
+import { authUser, getUserProfile } from '../controllers/userControllers.js'
 
 router.route('/login').post(authUser)
+router.route('/profile').get(protect, getUserProfile)
 
 export default router
