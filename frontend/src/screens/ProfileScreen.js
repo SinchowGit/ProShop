3011ac3fbe22791a240b947d1cfb68aecbb6805a
Saurabh Kumar <saurav123sau123@gmail.com>
2022-05-24 16:30:18 +0530

    Profile screen added, Update User Profile -2

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect} from 'react'
-import { Link, useLocation, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { Form, Button, Row, Col} from 'react-bootstrap'
 import { useSelector, useDispatch } from 'react-redux'
 import Message from '../components/Message'
