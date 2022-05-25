b3c22981f63b76e8c14717f2b155ff469b542070
Saurabh Kumar <saurav123sau123@gmail.com>
2022-05-25 12:07:00 +0530

    Get order by Id (Backend)

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -1,9 +1,9 @@
 import asyncHandler from 'express-async-handler'
 import Order from '../model/orderModel.js'
 
-// @desc    create a order
+// @desc    Create a order
 // @route   POST /api/orders
-// @access  Public
+// @access  Private
 const createOrder = asyncHandler( async (req,res) => {
     const { orderItems, shippingAddress, paymentMethod, itemsPrice, taxPrice, shippingPrice, totalPrice } = req.body
 
@@ -23,11 +23,23 @@ const createOrder = asyncHandler( async (req,res) => {
             shippingPrice,
             totalPrice
         })
-
         await order.save()
-
         res.status(201).json(order)
     }
 })
 
-export { createOrder }
+// @desc    Create order by Id
+// @route   GET /api/orders/:id
+// @access  Private
+const getOrderById = asyncHandler( async (req,res) => {
+    const orderId = req.params.id
+    const order = await Order.findById(orderId).populate('user', 'name email')
+    if(order){
+        res.json(order)
+    }else{
+        res.status(404)
+        throw new Error('Order not found')
+    }
+})
+
+export { createOrder, getOrderById }
