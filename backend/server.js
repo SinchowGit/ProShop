7971fc0bc7bbf376bab9c8da3eb3b1d28e6c1ee7
Saurabh Kumar <saurav123sau123@gmail.com>
2022-05-28 20:24:36 +0530

    Morgan added

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import products from './data/products.js'
 import connectDB from './config/db.js'
 import colors from 'colors'
 import path from 'path'
+import morgan from 'morgan'
 
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
@@ -16,6 +17,10 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
 
 app.use(express.json())        // Body Parser
 
