5596e3a3b7efb2f0c310e9d4fcf73fedb3c5e0b6
Saurabh Kumar <saurav123sau123@gmail.com>
2022-05-28 13:01:14 +0530

    Multer setup

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,39 @@
+import express from "express"
+import { path } from 'path'
+import multer from 'multer'
+
+const router = express.Router()
+
+const storage = multer.diskStorage({
+    destination(req, file, cb){
+        cb(null, 'uploads/')
+    },
+    filename(req, file, cb){
+        cb(null, `${this.file.fieldname}-${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+function checkFileType (file, cb) {
+    const filetype = /jpg|jpeg|png/
+    const extname = filetype.test(path.extname(file.originalname).toLowerCase())
+    const mimetype = filetype.test(file.mimetype)
+
+    if(extname && mimetype){
+        cb(null, true)
+    }else{
+        cb('Image only!')
+    }
+}
+
+const upload = multer({
+    storage,
+    fileFilter: function(req, file, cb){
+        checkFileType(file, cb)
+    }
+})
+
+router.post('/', upload.single('image'),(req,res) => {
+    res.send(`/${req.file.path}`)
+})
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,13 @@ import dotenv from 'dotenv'
 import products from './data/products.js'
 import connectDB from './config/db.js'
 import colors from 'colors'
+import path from 'path'
 
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
+
 import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 
 dotenv.config();
@@ -19,8 +22,11 @@ app.use(express.json())        // Body Parser
 app.use('/api/products',productRoutes)
 app.use('/api/users',userRoutes)
 app.use('/api/orders',orderRoutes)
+app.use('/api/upload',uploadRoutes)
 
 app.get('/api/config/paypal',(req,res)=> res.send(process.env.PAYPAL_CLIENT_ID))
+
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 app.use(errorHandler)
